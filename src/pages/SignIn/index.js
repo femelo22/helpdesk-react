21fde90d7609ce8c4f7ee0ba73bb67227cb0afc0
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2023-02-23 22:41:12 -0300

    feat: page signin form

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,36 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './signin.css';
+
+import logo from '../../assets/logo.png';
+
+
 export default function SinIn() {
+
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault(); // para nao atualizar a pagina (comportamento do formulario form)
+    alert('Tentou logar')
+  }
+
   return (
-    <div>
-      <h1>Página de Login</h1>
+    <div className='container-center'>
+      <div className='login'>
+        <div className='login-area'>
+          <img src={logo} alt="Logo Usuario" />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Entrar</h1>
+          <input type="text" placeholder='email@email.com' value={email} onChange={ (e) => setEmail(e.target.value) }/>
+          <input type="password" placeholder='********' value={password} onChange={ (e) => setPassword(e.target.value)}/>
+          <button type='submit'>Acessar</button>
+        </form>
+
+        <Link to='/register'>Criar uma conta</Link>
+      </div>
     </div>
   );
 }
