4c123411ad790f16bd5d363d8dd53c1a7eab7361
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2023-03-01 18:09:55 -0300

    feat: sidebar component

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,127 +1,129 @@
-
 import { useState, createContext, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import firebase from '../services/firebaseConnection';
 
 export const AuthContext = createContext({});
 
-function AuthProvider({ children }){
+function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   const [loadingAuth, setLoadingAuth] = useState(false);
   const [loading, setLoading] = useState(true);
 
   //Fazendo login do usuario
-  async function login(email, password){
+  async function login(email, password) {
     setLoadingAuth(true);
 
-    await firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(async (value)=> {
-      let uid = value.user.uid;
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
 
-      const userProfile = await firebase
-      .firestore()
-      .collection('users')
-      .doc(uid)
-      .get();
-
-      let data = {
-        uid: uid,
-        nome: userProfile.data().nome,
-        avatarUrl: userProfile.data().avatarUrl,
-        email: value.user.email
-      };
-
-      setUser(data);
-      storageUser(data);
-      setLoadingAuth(false);
-      toast.success('Bem Vindo!');
-    })
-    .catch((error)=>{
-      console.log(error);
-      toast.error('Ops, algo deu errado!');
-      setLoadingAuth(false);
-    })
-  }
-
-  //Cadastrando um novo usuario
-  async function createUser(email, password, nome){
-    setLoadingAuth(true);
-
-    await firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then( async (value)=>{
-      let uid = value.user.uid;
-
-      await firebase.firestore().collection('users')
-      .doc(uid).set({
-        nome: nome,
-        avatarUrl: null,
-      })
-      .then( () => {
+        const userProfile = await firebase
+          .firestore()
+          .collection('users')
+          .doc(uid)
+          .get();
 
         let data = {
           uid: uid,
-          nome: nome,
+          nome: userProfile.data().nome,
+          avatarUrl: userProfile.data().avatarUrl,
           email: value.user.email,
-          avatarUrl: null
         };
 
         setUser(data);
         storageUser(data);
         setLoadingAuth(false);
-        toast.success('Usuário salvo com sucesso.');
+        toast.success('Bem Vindo!');
       })
-    })
-    .catch((error)=>{
-      console.log(error);
-      toast.error('Ops, algo deu errado!');
-      setLoadingAuth(false);
-    })
+      .catch((error) => {
+        console.log(error);
+        toast.error('Ops, algo deu errado!');
+        setLoadingAuth(false);
+      });
+  }
+
+  //Cadastrando um novo usuario
+  async function createUser(email, password, nome) {
+    setLoadingAuth(true);
+
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(async (value) => {
+        let uid = value.user.uid;
+
+        await firebase
+          .firestore()
+          .collection('users')
+          .doc(uid)
+          .set({
+            nome: nome,
+            avatarUrl: null,
+          })
+          .then(() => {
+            let data = {
+              uid: uid,
+              nome: nome,
+              email: value.user.email,
+              avatarUrl: null,
+            };
+
+            setUser(data);
+            storageUser(data);
+            setLoadingAuth(false);
+            toast.success('Usuário salvo com sucesso.');
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error('Ops, algo deu errado!');
+        setLoadingAuth(false);
+      });
   }
 
   //Logout do usuario
-  async function signOut(){
+  async function signOut() {
     await firebase.auth().signOut();
     localStorage.removeItem('SistemaUser');
     setUser(null);
   }
 
-
-  function storageUser(data){
+  function storageUser(data) {
     localStorage.setItem('SistemaUser', JSON.stringify(data));
   }
 
-  useEffect(()=>{
-
-    function loadStorage(){
+  useEffect(() => {
+    function loadStorage() {
       const storageUser = localStorage.getItem('SistemaUser');
 
-      if(storageUser){
+      if (storageUser) {
         setUser(JSON.parse(storageUser));
         setLoading(false);
       }
-  
+
       setLoading(false);
     }
-    
+
     loadStorage();
+  }, []);
 
-  }, [])
-
-  return(
-    <AuthContext.Provider 
-    value={{ 
-      signed: !!user,  
-      user, 
-      loading, 
-      login,
-      signOut,
-      createUser,
-      loadingAuth
-    }}
+  return (
+    <AuthContext.Provider
+      value={{
+        signed: !!user,
+        user,
+        loading,
+        login,
+        signOut,
+        createUser,
+        loadingAuth,
+      }}
     >
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
 
 export default AuthProvider;
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,15 @@
-import { useContext } from "react";
-import { AuthContext } from "../../contexts/auth";
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/auth';
+import Header from '../../components/Header';
 
 export default function Dashboard() {
-
   const { signOut } = useContext(AuthContext);
- 
+
   return (
     <div>
+      <Header />
       <h1>Dashboard</h1>
       <button onClick={() => signOut()}> Sair </button>
     </div>
-  )
+  );
 }
